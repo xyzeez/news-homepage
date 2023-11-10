ca5e2ce4f577e43e3dd0d23e2b3623c988b5c733
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-11-10 04:41:41 +0300

    Modified header and managed nav close functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Elements
+const nav = document.querySelector('.nav');
+const navLinks = Array.from(document.querySelectorAll('.nav__item'));
+const navList = document.querySelector('.nav__list');
+const navHamburger = document.querySelector('.nav__hamburger');
+const openHamburger = document.querySelector('#hamburgerOpen');
+const closeHamburger = document.querySelector('#hamburgerClose');
 
+// Control Nav closing
+navList.addEventListener('click', (e) => {
+  const cursorTarget = e.target.closest('.nav__item');
+  if (!navLinks.includes(cursorTarget)) return;
+  closeHamburger.checked = true;
+});
